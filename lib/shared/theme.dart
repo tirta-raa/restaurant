0b16204682431aad99ac4fdfd78778c1ef13116a
guakoko <gua.koko1@gmail.com>
2021-08-06 09:33:50 +0700

    menyelesaikan theme.dart

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,1 +1,15 @@
 part of 'shared.dart';
+
+Color mainColor = 'FA4A0C'.toColor();
+Color greyColor = 'F2F2F2'.toColor();
+
+TextStyle blackFontStyle1 = GoogleFonts.poppins()
+    .copyWith(color: Colors.black, fontSize: 34, fontWeight: FontWeight.w700);
+
+TextStyle blackFontStyle2 = GoogleFonts.poppins()
+    .copyWith(color: Colors.black, fontSize: 34, fontWeight: FontWeight.w600);
+
+TextStyle blackFontStyle3 = GoogleFonts.poppins()
+    .copyWith(color: Colors.black, fontWeight: FontWeight.w600);
+
+const double defaultMargin = 50;
